8a1727dddd092046e12b719ed5be4f7e143d7140
Danillo Ilggner <danilloilggner@gmail.com>
2022-03-09 19:49:23 -0300

    add dataAsSet getter

--- lib/controllers/api_manager.dart
+++ lib/controllers/api_manager.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
+import 'dart:io';
 
 class ApiManager {
   static final url =
       Uri.parse('https://desafio-mobile.nyc3.digitaloceanspaces.com/movies');
 
-  static Future<String> get getData async {
-    final response = await get(url);
-    if (response.statusCode == 200) {
+  static Future<String> get data async {
+    try {
+      final response = await get(url);
       return response.body;
-    } else {
-      return response.statusCode.toString();
+    } on HttpException {
+      return 'erro';
     }
+  }
+
+  static Future<Set> get dataAsSet async {
+    final data = await ApiManager.data;
+    final dataDecoded = json.decode(data);
+    return Set.from(dataDecoded);
   }
 }
