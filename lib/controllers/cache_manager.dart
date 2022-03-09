3dd6c749554a9a4339183db64efc545e45351b0a
Danillo Ilggner <danilloilggner@gmail.com>
2022-03-09 16:14:51 -0300

    ignore managers for now

--- lib/controllers/cache_manager.dart
+++ lib/controllers/cache_manager.dart
@@ -1,29 +1,27 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
 class CacheManager {
-  static late SharedPreferences _prefs;
+  // static late SharedPreferences _prefs;
 
-  static Future<bool> get isDataCached async {
-    _prefs = await SharedPreferences.getInstance();
-    bool cached = _prefs.getBool('cached') ?? false;
-    return cached;
-  }
+  // static Future<bool> get isDataCached async {
+  //   _prefs = await SharedPreferences.getInstance();
+  //   bool cached = _prefs.getBool('cached') ?? false;
+  //   return cached;
+  // }
 
-  static Future<void> cacheData(String data) async {
-    _prefs = await SharedPreferences.getInstance();
-    _prefs.setString('data', data);
-    _prefs.setBool('cached', true);
-  }
+  // static Future<void> cacheData(String data) async {
+  //   _prefs = await SharedPreferences.getInstance();
+  //   _prefs.setString('data', data);
+  //   _prefs.setBool('cached', true);
+  // }
 
-  static Future<String> get cache async {
-    _prefs = await SharedPreferences.getInstance();
-    final data = _prefs.getString('data') ?? 'No cached data.';
-    return data;
-  }
+  // static Future<String> get cache async {
+  //   _prefs = await SharedPreferences.getInstance();
+  //   final data = _prefs.getString('data') ?? 'No cached data.';
+  //   return data;
+  // }
 
-  static Future<void> get eraseCache async {
-    _prefs = await SharedPreferences.getInstance();
-    _prefs.remove('data');
-    _prefs.setBool('cached', false);
-  }
+  // static Future<void> get eraseCache async {
+  //   _prefs = await SharedPreferences.getInstance();
+  //   _prefs.remove('data');
+  //   _prefs.setBool('cached', false);
+  // }
 }
--- lib/controllers/data_manager.dart
+++ lib/controllers/data_manager.dart
@@ -5,28 +5,28 @@ import 'package:desafio_tokenlab/controllers/cache_manager.dart';
 import 'package:desafio_tokenlab/models/movie.dart';
 
 class DataManager {
-  static Future<String> get dataAsString async {
-    final isCached = await CacheManager.isDataCached;
-    if (isCached) {
-      return CacheManager.cache;
-    }
-    final data = await ApiManager.getData;
-    CacheManager.cacheData(data);
-    return data;
-  }
+  // static Future<String> get dataAsString async {
+  //   final isCached = await CacheManager.isDataCached;
+  //   if (isCached) {
+  //     return CacheManager.cache;
+  //   }
+  //   final data = await ApiManager.getData;
+  //   CacheManager.cacheData(data);
+  //   return data;
+  // }
 
-  final _movies = <dynamic>[];
+  // final _movies = <dynamic>[];
 
-  //List<Movie> get movies => List.unmodifiable(_movies);
+  // //List<Movie> get movies => List.unmodifiable(_movies);
 
-  Future<List<dynamic>> get _decodeData async => jsonDecode(await dataAsString);
+  // Future<List<dynamic>> get _decodeData async => jsonDecode(await dataAsString);
 
-  Future<List<Object?>> get movies async {
-    var decodedData = await _decodeData;
+  // Future<List<Object?>> get movies async {
+  //   var decodedData = await _decodeData;
 
-    decodedData = decodedData.map((e) => Movie.fromMap(e)).toList();
+  //   decodedData = decodedData.map((e) => Movie.fromMap(e)).toList();
 
-    _movies.addAll(decodedData);
-    return _movies;
-  }
+  //   _movies.addAll(decodedData);
+  //   return _movies;
+  // }
 }
