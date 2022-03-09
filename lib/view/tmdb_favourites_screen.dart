75ab1a122e619604bfb0998a0a024def89e3d997
Danillo Ilggner <danilloilggner@gmail.com>
2022-03-09 15:36:44 -0300

    screen return movies list

--- lib/view/tmdb_favourites_screen.dart
+++ lib/view/tmdb_favourites_screen.dart
@@ -1,30 +1,14 @@
-import 'package:desafio_tokenlab/controllers/api_manager.dart';
-import 'package:desafio_tokenlab/controllers/data_manager.dart';
+import 'package:desafio_tokenlab/view/movies_list.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 
 class TmdbFavouritesScreen extends StatelessWidget {
   const TmdbFavouritesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: FutureBuilder(
-      future: DataManager().movies,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          //print(snapshot.data!);
-          //print(snapshot.data!);
-          print(snapshot);
-          return const Center();
-        } else if (snapshot.hasError) {
-          snapshot.error.toString();
-          return const Center();
-        }
-        return Center(
-          child: Text(snapshot.error.toString()),
-        );
-      },
-    ));
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 3, 37, 65),
+      body: MoviesList(),
+    );
   }
 }
