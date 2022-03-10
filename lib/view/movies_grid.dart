88531c5fc0c851f66fca717c8b7adba6107f1cb4
Danillo Ilggner <danilloilggner@gmail.com>
2022-03-10 15:49:14 -0300

    fixed futurebuilder

--- lib/view/movies_grid.dart
+++ lib/view/movies_grid.dart
@@ -1,29 +1,30 @@
-import 'package:desafio_tokenlab/controllers/data_manager.dart';
-import 'package:desafio_tokenlab/models/movie.dart';
+import 'package:desafio_tokenlab/controllers/data_helper.dart';
 import 'package:desafio_tokenlab/view/movie_card.dart';
 import 'package:flutter/material.dart';
 
 class MoviesGrid extends StatelessWidget {
   MoviesGrid({Key? key}) : super(key: key);
 
-  final dataManager = DataManager.getInstance;
+  final dataHelper = DataHelper.getInstance;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: dataManager.movies,
+        future: DataHelper.movies,
         builder: (context, AsyncSnapshot<List> movies) {
           if (movies.connectionState == ConnectionState.done) {
-            return GridView.builder(
-              // itemBuilder: (_, __) => const MovieCard(),
-
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2),
-
-              itemBuilder: (_, index) => MovieCard(movie: movies.data![index]),
-              itemCount: movies.data!.length,
-              padding: const EdgeInsets.only(top: 10, bottom: 10),
-            );
+            if (!movies.hasError) {
+              return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (_, index) =>
+                    MovieCard(movie: movies.data![index]),
+                itemCount: movies.data!.length,
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+              );
+            } else {
+              return Text(movies.error.toString());
+            }
           } else {
             return const Center(child: CircularProgressIndicator());
           }
